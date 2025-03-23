d1c0dcd8d7926b7c252484c2acb78b8f00f8f63f
ALLANOSAN <allanstryper@live.com>
2025-03-22 23:11:59 -0300

    update

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { registerRootComponent } from 'expo';
+import App from '../App';// Certifique-se de que o arquivo App.tsx está no nível raiz do projeto
+
+
+// Registra o componente raiz
+registerRootComponent(App);
