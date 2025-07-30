7926c8ebb7047674effabeadf7c29bb1387b724d
ALLANOSAN <allanstryper@live.com>
2025-07-29 22:48:49 -0300

    update

--- android/app/src/main/java/com/allanosan/audioplayerapp/MainActivity.kt
+++ android/app/src/main/java/com/allanosan/audioplayerapp/MainActivity.kt
@@ -1,27 +1,7 @@
 package com.allanosan.audioplayerapp
 
-import android.os.Build
-import android.os.Bundle
-
 import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
-
-import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String = "main"
-
-    override fun createReactActivityDelegate(): ReactActivityDelegate {
-        return ReactActivityDelegateWrapper(
-            this,
-            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-            DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-        )
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
--- android/app/src/main/java/com/allanosan/audioplayerapp/MainApplication.kt
+++ android/app/src/main/java/com/allanosan/audioplayerapp/MainApplication.kt
@@ -1,61 +1,30 @@
 package com.allanosan.audioplayerapp
 
 import android.app.Application
-import android.content.res.Configuration
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
-
-import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-        this,
+    override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages.toMutableList()
-            // Packages that cannot be autolinked yet can be added manually here
-    
-            // Filtrar pacotes excluídos
-            val excludedPackages = com.allanosan.AudioPlayerApp.excludedpackages.ExcludedPackages.getExcludedPackages()
-            return packages.filter { pkg -> 
-                !excludedPackages.contains(pkg.javaClass.name)
-            }
-          }
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Pacotes que não podem ser autolinkados ainda podem ser adicionados manualmente aqui
+                }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+            override fun getJSMainModuleName(): String = "index"
 
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-  )
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
 
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun onCreate() {
+        super.onCreate()
     }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
-
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-  }
 }
